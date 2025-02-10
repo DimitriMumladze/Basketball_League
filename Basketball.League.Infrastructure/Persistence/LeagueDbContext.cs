26e1e38bec021e2779d346e70bf38ea8b1736982
Dimitri <kenwo.dm@gmail.com>
2025-02-10 12:39:10 +0400

    dbset

---
+++ Basketball.League.Infrastructure/Persistence/LeagueDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Basketball.League.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Basketball.League.Infrastructure.Persistence;
+
+internal class LeagueDbContext(DbContextOptions<LeagueDbContext> options) : DbContext(options)
+{
+    internal DbSet<City> Cities { get; set; }
+    internal DbSet<Owner> Owners { get; set; }
+    internal DbSet<Player> Players { get; set; }
+    internal DbSet<State> States { get; set; }
+    internal DbSet<Team> Teams { get; set; }
+    internal DbSet<TeamOwner> TeamOwners { get; set; }
+}
